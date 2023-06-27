f1347b55d79f259c700a4369d43a2e66aac51681
iAmir <hhm6@yahoo.com>
2023-06-27 11:31:54 +0330

    use "prettier" formatter to format js files

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-'use strict';
+"use strict";
 
 // ensure process is running on linux
-if(process.platform !== 'linux') {
-	throw new Error('linux-user must be running on Linux');
+if (process.platform !== "linux") {
+  throw new Error("linux-user must be running on Linux");
 }
 
 // ensure process is running as root
-if(!(process.getuid && process.getuid() === 0)) {
-	console.warn('linux-user is NOT running as root, some functions may not work!');
+if (!(process.getuid && process.getuid() === 0)) {
+  console.warn(
+    "linux-user is NOT running as root, some functions may not work!"
+  );
 }
 
-var lib = require('./lib/user');
-lib.promise = require('./lib/promise')
+var lib = require("./lib/user");
+lib.promise = require("./lib/promise");
 
 module.exports = lib;
