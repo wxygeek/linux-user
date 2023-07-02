a6966d7b647af4a430b93cecce0cdb5790d40d88
iAmir <hhm6@yahoo.com>
2023-07-02 10:19:00 +0330

    make shell argument optional

--- index.d.ts
+++ index.d.ts
@@ -2,15 +2,15 @@ export type ErrorType = undefined | string | Error | null;
 
 export interface UserCreationArgs {
   username: string;
-  shell: string | null;
   create_home: boolean;
+  shell?: string | null;
   home_dir?: string;
   expiredate?: string | Date;
   skel?: string;
   system?: boolean;
   selinux_user?: boolean;
   other_args?: string;
-};
+}
 
 export interface UserInfo {
   username: string;
@@ -20,14 +20,14 @@ export interface UserInfo {
   fullname: string;
   homedir: string;
   shell: string;
-};
+}
 
 export interface GroupInfo {
   groupname: string;
   password: string;
   gid: number;
   members: string[];
-};
+}
 
 export interface UserExpirationInfo {
   changed: Date;
@@ -37,7 +37,7 @@ export interface UserExpirationInfo {
   minDays: number;
   maxDays: number;
   warnDays: number;
-};
+}
 
 export interface UserExpirationEditArgs {
   lastday: Date | string;
@@ -46,23 +46,70 @@ export interface UserExpirationEditArgs {
   minDays: number;
   maxDays: number;
   warnDays: number;
-};
+}
 
 export default class LinuxUser {
-  static addUser(args: UserCreationArgs | string, callback: (error: ErrorType, user?: UserInfo | null) => void): void;
-  static removeUser(username: string, callback: (error: ErrorType) => void): void;
-  static getUserGroups(username: string, callback: (error: ErrorType, groups?: string[]) => void): void;
-  static getUsers(callback: (error: ErrorType, users?: UserInfo[]) => void): void;
-  static getUserInfo(username: string, callback: (error: ErrorType, user?: UserInfo) => void): void;
-  static setPassword(username: string, password: string, callback: (error: ErrorType) => void): void;
-  static addGroup(groupName: string, callback: (error: ErrorType, group?: GroupInfo | null) => void): void;
-  static removeGroup(groupName: string, callback: (error: ErrorType) => void): void;
-  static getGroups(callback: (error: ErrorType, groups?: GroupInfo[]) => void): void;
-  static getGroupInfo(groupName: string, callback: (error: ErrorType, group?: GroupInfo | null) => void): void;
-  static addUserToGroup(username: string, groupName: string, callback: (error: ErrorType) => void): void;
-  static getExpiration(username: string, callback: (error: ErrorType, data?: UserExpirationInfo) => void): void;
-  static setExpiration(username: string, args: UserExpirationEditArgs, callback: (error: ErrorType, data: any) => void): void;
-  static verifySSHKey(key: string, callback: (error: ErrorType, done?: boolean) => void): void;
-  static addSSHtoUser(username: string, key: string, callback: (error: ErrorType, done?: boolean) => void): void;
+  static addUser(
+    args: UserCreationArgs | string,
+    callback: (error: ErrorType, user?: UserInfo | null) => void
+  ): void;
+  static removeUser(
+    username: string,
+    callback: (error: ErrorType) => void
+  ): void;
+  static getUserGroups(
+    username: string,
+    callback: (error: ErrorType, groups?: string[]) => void
+  ): void;
+  static getUsers(
+    callback: (error: ErrorType, users?: UserInfo[]) => void
+  ): void;
+  static getUserInfo(
+    username: string,
+    callback: (error: ErrorType, user?: UserInfo) => void
+  ): void;
+  static setPassword(
+    username: string,
+    password: string,
+    callback: (error: ErrorType) => void
+  ): void;
+  static addGroup(
+    groupName: string,
+    callback: (error: ErrorType, group?: GroupInfo | null) => void
+  ): void;
+  static removeGroup(
+    groupName: string,
+    callback: (error: ErrorType) => void
+  ): void;
+  static getGroups(
+    callback: (error: ErrorType, groups?: GroupInfo[]) => void
+  ): void;
+  static getGroupInfo(
+    groupName: string,
+    callback: (error: ErrorType, group?: GroupInfo | null) => void
+  ): void;
+  static addUserToGroup(
+    username: string,
+    groupName: string,
+    callback: (error: ErrorType) => void
+  ): void;
+  static getExpiration(
+    username: string,
+    callback: (error: ErrorType, data?: UserExpirationInfo) => void
+  ): void;
+  static setExpiration(
+    username: string,
+    args: UserExpirationEditArgs,
+    callback: (error: ErrorType, data: any) => void
+  ): void;
+  static verifySSHKey(
+    key: string,
+    callback: (error: ErrorType, done?: boolean) => void
+  ): void;
+  static addSSHtoUser(
+    username: string,
+    key: string,
+    callback: (error: ErrorType, done?: boolean) => void
+  ): void;
   static validateUsername(username: string): boolean;
-};
+}
